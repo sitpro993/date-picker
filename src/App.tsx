610ffa1ef568752ac836d79dd96a295ab081052e
hongnv <hongnv@gviet.vn>
2024-03-11 20:10:20 +0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import RangePicker, { type RangePickerProps } from "./PickerInput/RangePicker";
+import momentGenerateConfig from "./generate/moment";
+import en from "./locale/en_US";
+import logo from "./logo.svg";
+import "./App.css";
+import { Moment } from "moment";
 
 function App() {
+  const [value, setValue] = React.useState<
+    [Moment | null, Moment | null] | null
+  >([null, null]);
+
+  const onChange = (
+    newValue: [Moment | null, Moment | null] | null,
+    formatStrings?: string[]
+  ) => {
+    console.log("Change:", newValue, formatStrings);
+    setValue(newValue);
+  };
+
+  const onCalendarChange = (
+    newValue: [Moment | null, Moment | null] | null,
+    formatStrings?: string[]
+  ) => {
+    console.log("Calendar Change:", newValue, formatStrings);
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +40,17 @@ function App() {
         >
           Learn React
         </a>
+
+        <RangePicker<Moment>
+          value={value}
+          picker="date"
+          generateConfig={momentGenerateConfig}
+          onChange={onChange}
+          onCalendarChange={onCalendarChange}
+          locale={en}
+          placeholder={["Start date", "End date"]}
+          format="YYYY-MM-DD"
+        />
       </header>
     </div>
   );
